ed38d594860708e68a1d81345c0fa1a0efae5940
Frankie Dintino <fdintino@theatlantic.com>
2015-07-20 23:07:52 -0400

    old migrations => south_migrations

--- statictext/migrations/0001_initial.py
+++ statictext/migrations/0001_initial.py
@@ -1,39 +1,35 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-    
-    def forwards(self, orm):
-        
-        # Adding model 'StaticText'
-        db.create_table('statictext_statictext', (
-            ('content', self.gf('django.db.models.fields.TextField')(max_length=500, blank=True)),
-            ('URL', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-            ('enabled', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True)),
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-        ))
-        db.send_create_signal('statictext', ['StaticText'])
-    
-    
-    def backwards(self, orm):
-        
-        # Deleting model 'StaticText'
-        db.delete_table('statictext_statictext')
-    
-    
-    models = {
-        'statictext.statictext': {
-            'Meta': {'object_name': 'StaticText'},
-            'URL': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'content': ('django.db.models.fields.TextField', [], {'max_length': '500', 'blank': 'True'}),
-            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        }
-    }
-    
-    complete_apps = ['statictext']
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sites', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='StaticText',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('enabled', models.BooleanField(default=False, help_text=b'Display on Site')),
+                ('content', models.TextField(max_length=500, blank=True)),
+                ('url', models.URLField(verbose_name=b'URL', blank=True)),
+                ('slug', models.SlugField()),
+                ('layout', models.CharField(default=b'', max_length=100, blank=True)),
+                ('site', models.ForeignKey(to='sites.Site')),
+            ],
+            options={
+                'verbose_name': 'Text Snippet',
+                'verbose_name_plural': 'Text Snippets',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='statictext',
+            unique_together=set([('slug', 'site')]),
+        ),
+    ]
