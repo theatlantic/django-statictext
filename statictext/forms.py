4dd8a1cbb1aa56e4cf970c880e613cfaddfbdbf5
Chris Barna <cbarna@theatlantic.com>
2016-06-08 10:01:36 -0400

    Add "with layout" form.

--- statictext/forms.py
+++ statictext/forms.py
@@ -7,3 +7,8 @@ class StaticTextForm(forms.ModelForm):
     class Meta:
         model = StaticText
         fields = ("enabled", "content", "url",)
+
+
+class StaticTextWithLayoutForm(StaticTextForm):
+    class Meta(StaticTextForm.Meta):
+        fields = StaticTextForm.Meta.fields + ("layout",)
