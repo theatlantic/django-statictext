b551d3b1a6538d4c36eaaf7458412f3762bd66cc
Portia Burton <plburton@gmail.com>
2017-06-20 14:06:56 -0400

    DEV-20790: Added field to StaticText model

--- statictext/migrations/0002_statictext_kicker.py
+++ statictext/migrations/0002_statictext_kicker.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='statictext',
             name='kicker',
-            field=models.CharField(default=b'', max_length=100, blank=True),
+            field=models.CharField(default=b'', max_length=50, blank=True),
         ),
     ]
